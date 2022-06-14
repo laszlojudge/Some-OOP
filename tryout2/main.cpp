a1af2726f5467d1085e428d56c4ebb005707b302
Laszlo Biro <laszlojudge@gmail.com>
2022-06-14 16:32:22 +0200

    Initial Commit

---
+++ tryout2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tryout2
+//
+//  Created by Biro Laszlo on 14.06.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
